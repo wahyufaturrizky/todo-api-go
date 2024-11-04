3a1c6f1e3d78186b62c127ad90cd8e0b51187ab3
wahyu.rizki <wahyu.rizki@accenture.com>
2024-11-04 13:11:31 +0700

    FEAT: getTodoByID

--- main.go
+++ main.go
@@ -19,6 +19,18 @@ func createTodo(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(todo)
 }
 
+func getTodoByID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range todos {
+			if item.ID == params["id"] {
+					w.Header().Set("Content-Type", "application/json")
+					json.NewEncoder(w).Encode(item)
+					return
+			}
+	}
+	w.WriteHeader(http.StatusNotFound)
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/todos", getTodos).Methods("GET")
